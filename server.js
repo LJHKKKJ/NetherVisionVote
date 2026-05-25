e91acf4ecb74dbeca88e9fe9e8b0e6a9854708a7
LJHKKKJ <galuskindavid20@gmail.com>
2026-05-25 14:45:46 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('.')); // Это заставит сервер отдавать твой index.html
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
